e2f41b668521ba9be91e6683b653602c6eb443b4
Jesus <jesusfercan@gmail.com>
2023-11-22 17:02:52 +0100

    optimize imports

--- src/main/java/com/example/demothymeleaf/entity/Associate.java
+++ src/main/java/com/example/demothymeleaf/entity/Associate.java
@@ -1,6 +1,10 @@
 package com.example.demothymeleaf.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
--- src/main/java/com/example/demothymeleaf/security/RecaptchaFilter.java
+++ src/main/java/com/example/demothymeleaf/security/RecaptchaFilter.java
@@ -6,14 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
